71000d0126567af2c30661ca1c4709f2284442de
Ahmet Yağız <ahmetanteryagiz@gmail.com>
2024-07-13 12:39:35 +0300

    Added Exit Car Animation

--- Assets/Code/DebugCarAnimationManager.cs
+++ Assets/Code/DebugCarAnimationManager.cs
@@ -6,15 +6,19 @@ using UnityEngine.Animations.Rigging;
 public class DebugCarAnimationManager : MonoBehaviour
 {
     private Animator animator;
+    public CharacterController characterController;
     public float cancelLeftHandRigDelay;
     public float activateLeftHandRigDelay;
     public Rig leftHandRig;
     public Animator carDoorAnimator;
     public Transform CarDriveTarget;
     public Transform EnterKenarTarget;
+    public Transform EnterCarPoint;
     public TwoBoneIKConstraint twoBoneIKConstraint;
     public Transform nextTarget; //sonraki tutacaðý nokta
     public RigBuilder rigBuilder;
+    private bool moveToSeat;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -28,14 +32,21 @@ public class DebugCarAnimationManager : MonoBehaviour
 
     public void CarExitAnimation()
     {
-        animator.SetTrigger("ExitCar");
+        StartCoroutine(ExitCarRoutine());
     }
 
+    #region Car Enter Animations
     IEnumerator EnterCarRoutine()
     {
+        animator.applyRootMotion = true;
+        characterController.enabled = false;
+        transform.position = EnterCarPoint.position;
+        Vector3 lookDir = new Vector3(EnterCarPoint.position.x, transform.position.y, EnterCarPoint.position.z);
+        transform.DOLookAt(lookDir, 0.5f);
         animator.SetTrigger("EnterCar");
-        Invoke(nameof(EnterCarTrigger), 1f);
+        Invoke(nameof(OpenDoorTrigger), 1f);
         Invoke(nameof(EnableHandWeight), 0.1f);
+        moveToSeat = true;
         Invoke(nameof(MoveToSide), 1.25f);
         Invoke(nameof(CloseDoorTrigger), 3f);
         Invoke(nameof(NextTarget),2f);
@@ -43,7 +54,7 @@ public class DebugCarAnimationManager : MonoBehaviour
         yield return null;
     }
 
-    void EnterCarTrigger()
+    void OpenDoorTrigger()
     {
         carDoorAnimator.SetTrigger("OpenDoor");
     }
@@ -68,12 +79,18 @@ public class DebugCarAnimationManager : MonoBehaviour
         DOTween.To(() => leftHandRig.weight, x => leftHandRig.weight = x, 0.5f, 1).SetEase(Ease.InSine);
     }
 
-
     void MoveToSide()
     {
         transform.DOMove(EnterKenarTarget.position, 0.5f).OnComplete(() =>
         {
-            Invoke(nameof(MoveToSeat), 0.5f);
+            if (moveToSeat)
+            {
+                Invoke(nameof(MoveToSeat), 0.5f);
+            }
+            else
+            {
+
+            }
         });
     }
 
@@ -82,5 +99,31 @@ public class DebugCarAnimationManager : MonoBehaviour
         EnableHandWeight();
         twoBoneIKConstraint.data.target = nextTarget;
         rigBuilder.Build();
+    }
+    #endregion
+
+    #region
+
+    IEnumerator ExitCarRoutine()
+    {
+        animator.SetTrigger("ExitCar");
+        Invoke(nameof(OpenDoorTrigger), 1f);
+        Invoke(nameof(EnableHandWeight), 0.1f);
+        moveToSeat = false;
+        Invoke(nameof(MoveToSide), 2.75f);
+        Invoke(nameof(CloseDoorTrigger), 3f);
+        //Invoke(nameof(NextTarget), 2f);
+        //Invoke(nameof(DisableHandWeight), 3.55f);
+        Invoke(nameof(ActivatePlayerBehaviors), 4);
+        yield return null;
+    }
+    #endregion
+
+    void ActivatePlayerBehaviors()
+    {
+        transform.position = EnterCarPoint.position;
+        leftHandRig.weight = 0;
+        animator.applyRootMotion = false;
+        characterController.enabled = true;
     }
 }
